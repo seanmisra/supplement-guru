e966d96d19e5d6f7f07344de82e3543436521b38
Sean Misra <seanmisra30@gmail.com>
2022-12-12 13:15:49 -0500

    Admin Panel - add supplements

--- src/App.js
+++ src/App.js
@@ -2,14 +2,74 @@ import './App.css';
 import Header from './components/Header';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Admin from './components/Admin';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [supplements, setSupplements] = useState([]);
+
+  useEffect(() => {
+    const getSupplements = async () => {
+      const supplementsFromServer = await fetchSupplements()
+      setSupplements(supplementsFromServer);
+    }
+
+    getSupplements();
+  }, [])
+
+  const fetchSupplements = async () => {
+    const res = await fetch('http://localhost:5000/supplements')
+    const data = await res.json()
+
+    return data;
+  }
+
+  const fetchSupplement = async (id) => {
+    const res = await fetch(`http://localhost:5000/supplements/${id}`)
+    const data = await res.json()
+
+    return data;
+  } 
+
+  const addSupplement = async(supplement) => {
+    const res = await fetch('http://localhost:5000/supplements', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(supplement)
+    })
+
+    const data = await res.json()
+    setSupplements([...supplements, data])
+  }
+
+  const editSupplement = async(supplement) => {
+    // update in memory
+    const res = await fetch(`http://localhost:5000/supplements/${supplement.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(supplement)
+    });
+
+    const data = await res.json()
+
+    // update locally
+    setSupplements(
+      supplements.map((thisSupp) => 
+        thisSupp.id === supplement.id ? supplement : this.supp
+      )
+    )
+  }
+
   return (
     <Router>
       <Header/>
       
       <Routes> 
-        <Route path='/adminPanel' element={<Admin/>}>
+        <Route path='/adminPanel' element={<Admin onAdd={addSupplement} />}>
 
         </Route>
       </Routes>
--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,7 +1,60 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
-const Admin = () => {
+const Admin = ({onAdd}) => {
+    const [suppName, setSuppName] = useState('')
+    const [suppDesription, setSuppDescription] = useState('')
+    const [suppTags, setSuppTags] = useState('')
+
+    const [suppNameDelete, setSuppNameDelete] = useState('')
+    const [adminPassword, setAdminPassword] = useState('')
+
+    const validatePassword = () => {
+        const adminPasswordVal = process.env.REACT_APP_ADMIN_UPDATE_PASSWORD;
+        return adminPassword === adminPasswordVal;
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const adminPasswordVal = process.env.REACT_APP_ADMIN_UPDATE_PASSWORD;
+
+        if (!validatePassword()) {
+            alert('Admin Password is incorrect');
+            return;
+        }
+
+        if (!suppName) {
+            alert('Supplement Name is required');
+            return;
+        }
+
+        if (!suppTags) {
+            alert('Tags are required');
+            return;
+        }
+
+        const parsedTags = suppTags.split(',');
+
+        if (parsedTags.length < 1) {
+            alert('At least one tag is required in a comma-separated format');
+            return;
+        }
+
+
+        onAdd({ 
+            name: suppName,
+            description: setSuppDescription,
+            tags: parsedTags
+        })
+
+        alert('Successfully added ' + suppName);
+        setSuppName('');
+        setSuppDescription('');
+        setSuppTags('');
+
+    }
+
     return(
         <div className='adminWrapper'> 
             <h1>
@@ -9,22 +62,43 @@ const Admin = () => {
             </h1>
             <p id="adminHomeLink"><Link to='/'>Back Home</Link></p>
 
-            <h3>Add Supplement</h3>
-            <div className='formWrapper'>
-                <label for='suppName'>Name: </label> 
-                <input id='suppName' name='suppName'/>
+            <label htmlFor='adminPassword'>Password: </label> 
+            <input required id='adminPassword' name='adminPassword'
+                value={adminPassword} onChange={(e) => setAdminPassword(e.target.value) }
+                />
+            
+            <h3>Add/Edit Supplement</h3>
+            <p>If supplement name is existing, then existing entry will be updated</p>
+            <form className='formWrapper' onSubmit={onSubmit}> 
+                <label htmlFor='suppName'>Name: </label> 
+                <input id='suppName' name='suppName'
+                value={suppName} onChange={(e) => setSuppName(e.target.value) }
+                />
                 <br/>
 
-                <label for='suppDescription'>Description: </label> 
-                <textarea type='textarea' id='suppDescription' name='suppDescription'/>
+                <label htmlFor='suppDescription'>Description: </label> 
+                <textarea type='textarea' id='suppDescription' name='suppDescription'
+                value={suppDesription} onChange={(e) => setSuppDescription(e.target.value)}
+                />
                 <br/>
 
-                <label for='suppTags'>Tags: </label> 
-                <input id='suppTags' name='suppTags'/>
+                <label htmlFor='suppTags'>Tags: </label> 
+                <input id='suppTags' name='suppTags'
+                value={suppTags} onChange={(e) => setSuppTags(e.target.value)}
+                />
                 <br/>
 
                 <button>Submit</button>
-            </div>  
+            </form>  
+
+            <h3>Delete Supplement</h3>
+            <p>If supplement name is found, that entry will be deleted</p>
+            <form>
+                <label htmlFor='suppNameDelete'>Name: </label> 
+                    <input id='suppNameDelete' name='suppNameDelete'
+                    value={suppNameDelete} onChange={(e) => setSuppNameDelete(e.target.value) }
+                    />
+            </form>
         </div>
     )
 }
